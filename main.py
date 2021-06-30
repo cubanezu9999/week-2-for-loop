c696ae4a617bede73383f06bcd28bb96633969be
cubanezu9999 <71436676+cubanezu9999@users.noreply.github.com>
2021-06-30 14:50:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+for x in range (0,10,1):
+  print(x)
+
+names = ["Tim","Jim","Tom","Phil"]
+for temp in names:
+  print(temp)
+  if temp ==("Tom"):
+     break
+for temp in range(0,4,1):
+  print(names[3])
